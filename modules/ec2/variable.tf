ca89b55134f045f9dbbd0afbaa33aa2933b81d72
chintakulavinay <chintakulavinay@outlook.com>
2026-08-10 16:27:37 +0530

    adding

---
+++ modules/ec2/variable.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+  description = "ec2_ami"
+  type        = string
+}
+
+variable "name" {
+  description = "name of ec2"
+  type        = string
+
+}
+
+variable "instance_type" {
+  description = "instance type"
+  type        = string
+
+}
---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+  description = "ec2_ami"
+  type        = string
+}
+
+variable "Name" {
+  description = "name of ec2"
+  type        = string
+
+}
+
+variable "instance_type" {
+  description = "instance type"
+  type        = string
+
+}
+
